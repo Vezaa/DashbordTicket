b0fab426419e8984675cb5a7b64d8b9dc04ca780
Vinicus Alamini <56409265+Vezaa@users.noreply.github.com>
2024-10-29 18:30:16 -0300

    Criando agrupamento
    Criando agrupamento

--- Controller/TicketController.cs
+++ Controller/TicketController.cs
@@ -1,6 +1,7 @@
 ﻿using Dashboard.Models;
 using Dashboard.Service;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
 
 namespace Dashboard.Controllers
 {
@@ -10,18 +11,66 @@ namespace Dashboard.Controllers
     {
         private TicketService _ticketService = ticketService;
 
-        [HttpGet("lista")]
+        [HttpGet("tickets/BuscarPorAnoMes")]
         public ActionResult<List<Ticket>> BuscarPorAnoMes(int mesAbertura, int anoAbertura, int mesEncerramento, int anoEncerramento)
         {
+
+            if (((mesAbertura > mesEncerramento) && anoAbertura == anoEncerramento || (anoAbertura > anoEncerramento)))
+            {
+                return BadRequest("O mes ou o ano de abertura não pode ser maior que o encerramento");
+            }
             var tickets = _ticketService.CarregarTicketsDoCsv();
             var ticketsFiltrados = tickets.Where(ticket =>
                 (ticket.DataAbertura.Year >= anoAbertura && ticket.DataAbertura.Month >= mesAbertura) &&
                 (ticket.DataEncerramento.Year <= anoEncerramento && ticket.DataEncerramento.Month <= mesEncerramento))
                 .ToList();
 
+            if (ticketsFiltrados.Count == 0)
+            {
+                return NotFound(new
+                {
+                    Mensagem = $"Não existem tickets nesse período Mes de Abertura: {mesAbertura} Ano de Abertura: {anoAbertura} a  Mes de encerramento: {mesEncerramento} Ano de Encerramento: {anoEncerramento}."
+                });
+            }
+
 
             return Ok(ticketsFiltrados);
         }
 
+        [HttpGet("agrupados/modulo")]
+        public ActionResult<List<Ticket>> GetTicketsAgrupadosPorModulo()
+        {
+            var tickets = _ticketService.CarregarTicketsDoCsv();
+            var ticketsAgrupadosPorModulo = tickets
+                .GroupBy(ticket => ticket.Modulo)
+                .Select(grupo => new
+                {
+                    Modulo = grupo.Key,
+                    Tickets = grupo.ToList()
+                })
+                .ToList();
+
+            return Ok(ticketsAgrupadosPorModulo);
+        }
+
+        [HttpGet("agrupados/cliente")]
+        public ActionResult<List<Ticket>> GetTicketsAgrupadosPorCliente()
+        {
+            var tickets = _ticketService.CarregarTicketsDoCsv();
+
+            var ticketsAgrupadosPorCliente = tickets
+                .GroupBy(ticket => ticket.Cliente)
+                .Select(grupo => new
+                {
+                    Cliente = grupo.Key,
+                    Tickets = grupo.ToList()
+                })
+                .ToList();
+
+            return Ok(ticketsAgrupadosPorCliente);
+        }
+
     }
+
+    
 }
